7a6c2b82107821770aa87fb7e6d8ae8dff0a8124
Reuben Wong <reuben.wong.ys@gmail.com>
2023-09-26 22:40:54 +0800

    client and query traits

--- src/api.rs
+++ src/api.rs
@@ -1,6 +1,10 @@
+mod client;
 mod endpoint;
 mod error;
 mod query;
 
 pub use self::error::ApiError;
 pub use self::error::BodyError;
+
+pub use self::client::AsyncClient;
+pub use self::client::Client;
