d4d89668dc1c504c4ed7d990cc523fd73f20866a
Reuben Wong <reuben.wong.ys@gmail.com>
2023-09-27 23:35:51 +0800

    re-order exports

--- src/api.rs
+++ src/api.rs
@@ -7,23 +7,17 @@ mod params;
 mod query;
 mod raw;
 
-pub mod endpoint_prelude;
-
-pub use self::error::ApiError;
-pub use self::error::BodyError;
-
 pub use self::client::AsyncClient;
 pub use self::client::Client;
 pub use self::client::RestClient;
 
 pub use self::endpoint::Endpoint;
 
-pub use self::params::FormParams;
-pub use self::params::ParamValue;
-pub use self::params::QueryParams;
+pub use self::error::ApiError;
+pub use self::error::BodyError;
 
-pub use self::query::AsyncQuery;
-pub use self::query::Query;
+pub use self::ignore::ignore;
+pub use self::ignore::Ignore;
 
 pub use self::paged::paged;
 pub use self::paged::LazilyPagedIter;
@@ -32,3 +26,13 @@ pub use self::paged::Pageable;
 pub use self::paged::Paged;
 pub use self::paged::Pagination;
 pub use self::paged::PaginationError;
+
+pub use self::params::FormParams;
+pub use self::params::ParamValue;
+pub use self::params::QueryParams;
+
+pub use self::query::AsyncQuery;
+pub use self::query::Query;
+
+pub use self::raw::raw;
+pub use self::raw::Raw;
