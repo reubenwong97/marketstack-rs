758c28468eebcaa09f8a8d9f3325b02b5c1d19db
Reuben Wong <reuben.wong.ys@gmail.com>
2023-10-13 22:44:52 +0800

    added eod type to deserialize and add test

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,4 @@
-use chrono::NaiveDate;
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 /// Basic struct that acts as dummy.
@@ -21,7 +21,7 @@ pub struct PaginationInfo {
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct EodDataItem {
     /// Exact date/time the given data was collected in ISO-8601 format.
-    date: NaiveDate,
+    date: DateTime<Utc>,
     /// Stock ticker symbol of the current data object.
     symbol: String,
     /// Exchange MIC identification associated with the current data object.
@@ -55,6 +55,47 @@ pub struct EodDataItem {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct EodData {
-    pub pagination_info: PaginationInfo,
+    pub pagination: PaginationInfo,
     pub data: Vec<EodDataItem>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::EodData;
+
+    #[test]
+    fn test_deserialize_eod() {
+        let json_data = r#"{
+        "pagination": {
+            "limit": 100,
+            "offset": 0,
+            "count": 100,
+            "total": 9944
+        },
+        "data": [
+            {
+                "open": 129.8,
+                "high": 133.04,
+                "low": 129.47,
+                "close": 132.995,
+                "volume": 106686703.0,
+                "adj_high": 133.04,
+                "adj_low": 129.47,
+                "adj_close": 132.995,
+                "adj_open": 129.8,
+                "adj_volume": 106686703.0,
+                "split_factor": 1.0,
+                "dividend": 0.0,
+                "symbol": "AAPL",
+                "exchange": "XNAS",
+                "date": "2021-04-09T00:00:00+0000"
+            }
+        ]
+    }"#;
+
+        let eod_data: EodData = serde_json::from_str(json_data).unwrap();
+        assert_eq!(eod_data.data[0].open, 129.8);
+        assert_eq!(eod_data.data[0].symbol, "AAPL");
+        assert_eq!(eod_data.pagination.limit, 100);
+    }
 }
