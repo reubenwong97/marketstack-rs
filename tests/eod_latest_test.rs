2b5d7482b9c26343b44c45758480f8bc9bfaae24
Reuben Wong <reuben.wong.ys@gmail.com>
2023-10-19 23:09:59 +0800

    add integration tests for eod/latest endpoint

--- tests/eod_latest_test.rs
+++ tests/eod_latest_test.rs
@@ -19,3 +19,73 @@ fn test_eod_latest() {
 
     assert_eq!(eod_result.data.len(), 1);
 }
+
+#[test]
+#[ignore]
+fn test_eod_latest_paged() {
+    let api_key = setup::setup_key();
+    let client = Marketstack::new_insecure("api.marketstack.com", api_key).unwrap();
+
+    let endpoint = eod::EodLatest::builder()
+        .symbol("AAPL")
+        .limit(5)
+        .unwrap()
+        .build()
+        .unwrap();
+    let eod_result: EodData = endpoint.query(&client).unwrap();
+
+    assert_eq!(eod_result.pagination.limit, 5);
+    assert_eq!(eod_result.data.len(), 1);
+}
+
+#[test]
+#[ignore]
+fn test_eod_latest_sorting() {
+    let api_key = setup::setup_key();
+    let client = Marketstack::new_insecure("api.marketstack.com", api_key).unwrap();
+
+    let endpoint = eod::EodLatest::builder()
+        .symbol("AAPL")
+        .sort(SortOrder::Ascending)
+        .build()
+        .unwrap();
+
+    let _: EodData = endpoint.query(&client).unwrap();
+}
+
+#[tokio::test]
+#[ignore]
+async fn test_async_eod_latest() {
+    let api_key = setup::setup_key();
+    let client = AsyncMarketstack::new_insecure("api.marketstack.com", api_key)
+        .await
+        .unwrap();
+
+    let endpoint = eod::EodLatest::builder().symbol("AAPL").build().unwrap();
+    let eod_result: EodData = endpoint.query_async(&client).await.unwrap();
+
+    assert_eq!(eod_result.pagination.limit, 100);
+    assert_eq!(eod_result.pagination.offset, 0);
+
+    assert_eq!(eod_result.data.len(), 1);
+}
+
+#[tokio::test]
+#[ignore]
+async fn test_async_eod_paged() {
+    let api_key = setup::setup_key();
+    let client = AsyncMarketstack::new_insecure("api.marketstack.com", api_key)
+        .await
+        .unwrap();
+
+    let endpoint = eod::EodLatest::builder()
+        .symbol("AAPL")
+        .limit(5)
+        .unwrap()
+        .build()
+        .unwrap();
+    let eod_result: EodData = endpoint.query_async(&client).await.unwrap();
+
+    assert_eq!(eod_result.pagination.limit, 5);
+    assert_eq!(eod_result.data.len(), 1);
+}
