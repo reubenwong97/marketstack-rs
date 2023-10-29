7aa84a290c135da4f15a6278d841815258c94c70
Reuben Wong <reuben.wong.ys@gmail.com>
2023-10-29 20:55:16 +0800

    Add unit tests for tickers params that come from eod

--- src/api/tickers.rs
+++ src/api/tickers.rs
@@ -4,8 +4,9 @@ use std::borrow::Cow;
 
 use derive_builder::Builder;
 
-use crate::api::endpoint_prelude::*;
 use crate::api::eod::Eod;
+use crate::api::paged::PaginationError;
+use crate::api::{endpoint_prelude::*, ApiError};
 
 /// Base for `tickers`.
 #[derive(Debug, Builder, Clone)]
@@ -14,6 +15,18 @@ pub struct Tickers<'a> {
     /// Ticker symbol.
     #[builder(setter(into), default)]
     ticker: Option<Cow<'a, str>>,
+    /// To filter your results based on a specific stock exchange, use this parameter to specify the MIC identification of a stock exchange. Example: `XNAS`
+    #[builder(setter(into), default)]
+    exchange: Option<Cow<'a, str>>,
+    /// Use this parameter to search stock tickers by name or ticker symbol.
+    #[builder(setter(into), default)]
+    search: Option<Cow<'a, str>>,
+    /// Pagination limit for API request.
+    #[builder(setter(name = "_limit"), default)]
+    limit: Option<PageLimit>,
+    /// Pagination offset value for API request.
+    #[builder(default)]
+    offset: Option<u64>,
     /// `Eod` struct being built, and held by the `Tickers` struct.
     #[builder(setter(into), default)]
     eod: Option<Eod<'a>>,
@@ -43,6 +56,35 @@ impl<'a> Endpoint for Tickers<'a> {
 
         endpoint.into()
     }
+
+    fn parameters(&self) -> QueryParams {
+        let mut params = QueryParams::default();
+
+        // NOTE: Not the most ergonomic way I want to go about this, but its okay for now since
+        // only one "extension" endpoint like `eod` or `splits` can be active per `tickers`
+        // endpoint query to Marketstack.
+        if let Some(eod) = &self.eod {
+            params = eod.parameters().clone();
+        }
+
+        // Push params from the `tickers` endpoint.
+        params
+            .push_opt("exchange", self.exchange.as_ref())
+            .push_opt("search", self.search.as_ref())
+            .push_opt("limit", self.limit.clone())
+            .push_opt("offset", self.offset);
+
+        params
+    }
+}
+
+impl<'a> TickersBuilder<'a> {
+    /// Limit the number of results returned.
+    pub fn limit(&mut self, limit: u16) -> Result<&mut Self, ApiError<PaginationError>> {
+        let new = self;
+        new.limit = Some(Some(PageLimit::new(limit)?));
+        Ok(new)
+    }
 }
 
 #[cfg(test)]
@@ -50,6 +92,7 @@ mod tests {
 
     use chrono::NaiveDate;
 
+    use crate::api::common::SortOrder;
     use crate::api::eod::Eod;
     use crate::api::tickers::Tickers;
     use crate::api::{self, Query};
@@ -120,6 +163,23 @@ mod tests {
                     .build()
                     .unwrap(),
             )
+            .build()
+            .unwrap();
+        api::ignore(endpoint).query(&client).unwrap();
+    }
+
+    #[test]
+    fn tickers_eod_params() {
+        let endpoint = ExpectedUrl::builder()
+            .endpoint("tickers/AAPL/eod")
+            .add_query_params(&[("sort", "ASC")])
+            .build()
+            .unwrap();
+        let client = SingleTestClient::new_raw(endpoint, "");
+
+        let endpoint = Tickers::builder()
+            .ticker("AAPL")
+            .eod(Eod::builder().sort(SortOrder::Ascending).build().unwrap())
             .build()
             .unwrap();
         api::ignore(endpoint).query(&client).unwrap();
